750147d3de438a4dd98c1a18aebb9faefd774de9
Candyrizz <rishavsingh076@gmail.com>
2025-06-25 09:19:15 +0530

    minor changes

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 #include <future>
 #include <chrono>
 
-int add(int a, int b)
+void worker (std::promise<int> resultPromise)
 {
+    std::cout << "Worker: Doing some work..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    return a + b;
+
+    int result = 42;
+    std::cout << "worker: work done!, setting result";
+    resultPromise.set_value(result);
 }
 
 int main()
 {
-    std::future<int> futureValue = std::async(std::launch::async, add, 1,2);
-    std::cout << "I am going!" << std::endl;
-    std::cout << futureValue.get() << std::endl;
+    std::promise<int> myPromise;
+    std::future<int> myFuture = myPromise.get_future();
+
+    std::thread t(worker, std::move(myPromise));
+
+    std::cout << "Main Waiting for result..." << std::endl;
+    int value = myFuture.get();
+    std::cout << "Result: " << value  << std::endl;
+
+    t.join();
 
 }
